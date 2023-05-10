dd4f40ca72a82aee6100d41c0ef1991b825a6a1f
alumno <alumno@DAM1-08>
2023-05-10 11:05:25 +0200

    2

--- src/Sampedro_Jesus.java
+++ src/Sampedro_Jesus.java
@@ -4,5 +4,6 @@ public class Sampedro_Jesus {
 	  public static void main(String[] args) {
 	        System.out.println("Hello world!");
 	        System.out.println("1");
+	        System.out.println("2");
 	    }
 }
